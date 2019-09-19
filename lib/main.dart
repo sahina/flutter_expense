f5e99a65e20091a550b944c016120737c218f421
Altug Sahin <altug@aecoffice.com>
2019-09-18 20:15:42 -0400

    added transaction model and simple layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,46 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New shoes',
+      amount: 55.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Groceries',
+      amount: 100.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text("Widget Playground"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+              width: double.infinity,
+              child: Card(
+                child: Text("CHART"),
+                elevation: 5,
+                color: Colors.blue,
+              )),
+          Column(
+            children: transactions.map((t) {
+              return Card(
+                child: Text(t.title),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
